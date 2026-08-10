1cad359dfd1047674999d67bc120829c73f576c6
ElPatrondelasOfertasMELI1 <conejotoystoys@gmail.com>
2026-08-10 10:29:34 -0600

    Crear prueba-auth.js

---
+++ prueba-auth.js
@@ -0,0 +1,136 @@
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+    getAuth,
+    signInWithEmailAndPassword,
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBo_wk-k8TrcSl0MQzQ0hoUCvAKre94hW0",
+
+    authDomain: "patronofertasweb.firebaseapp.com",
+
+    projectId: "patronofertasweb",
+
+    storageBucket: "patronofertasweb.firebasestorage.app",
+
+    messagingSenderId: "292338334268",
+
+    appId: "1:292338334268:web:9dbbafe00dd23ebb72e139"
+
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+
+console.log("🔥 PRUEBA AUTH CARGADA");
+
+
+const boton =
+    document.getElementById("entrarAdmin");
+
+const correo =
+    document.getElementById("correoAdmin");
+
+const password =
+    document.getElementById("passwordAdmin");
+
+const error =
+    document.getElementById("errorLogin");
+
+
+const login =
+    document.getElementById("loginAdmin");
+
+const panel =
+    document.getElementById("dashboardAdmin");
+
+
+boton?.addEventListener("click", async () => {
+
+    console.log("🔥 BOTÓN ENTRAR FUNCIONA");
+
+    const email =
+        correo.value.trim();
+
+    const pass =
+        password.value;
+
+    if (!email || !pass) {
+
+        error.textContent =
+            "⚠️ Escribe correo y contraseña";
+
+        return;
+
+    }
+
+    boton.disabled = true;
+
+    boton.textContent =
+        "⏳ ENTRANDO...";
+
+    error.textContent = "";
+
+    try {
+
+        console.log(
+            "🔐 Intentando:",
+            email
+        );
+
+        const resultado =
+            await signInWithEmailAndPassword(
+                auth,
+                email,
+                pass
+            );
+
+        console.log(
+            "✅ LOGIN FIREBASE:",
+            resultado.user.uid
+        );
+
+        error.textContent =
+            "✅ Autenticación correcta";
+
+    }
+
+    catch (e) {
+
+        console.error(
+            "❌ ERROR:",
+            e
+        );
+
+        error.textContent =
+            e.code +
+            " — " +
+            e.message;
+
+        boton.disabled = false;
+
+        boton.textContent =
+            "🔐 ENTRAR";
+
+    }
+
+});
+
+
+onAuthStateChanged(auth, user => {
+
+    console.log(
+        "👤 ESTADO:",
+        user
+    );
+
+});
